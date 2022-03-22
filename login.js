2212ec2845daa6eda623ba70b9e194446c2a10de
Ayomide Adekunle <davidadekunle88@gmail.com>
2022-03-21 23:12:20 -0500

    First commit.

---
+++ login.js
@@ -0,0 +1,24 @@
+// const baseURL = "https://www.coingecko.com/en/api"
+
+const fetchTrendingCoins = () => {
+    let trendingCoins = {}
+    const TrendingCoins = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+    fetch(TrendingCoins).then(response => {
+        return response.json();
+    }).then(data => {
+        trendingCoins = data;
+        console.log(trendingCoins);
+        Object.keys(data).forEach((key) => {
+            console.log(data[key].image)
+            let div = document.createElement("div");
+            let img = document.createElement("img");
+            div.setAttribute("class", "carousel-item")
+            img.setAttribute("src", data[key].image);
+            img.setAttribute("class", "d-block w-100");
+            div.appendChild(img);
+            let elem = document.getElementById("carousel-inner").appendChild(div);
+        });
+    }).catch(e => {
+        console.log(e);
+    });
+};
